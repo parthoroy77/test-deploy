b97e64893ca091d607a2dbd81115db216a700cbb
Partho Roy <roypartho772@gmail.com>
2024-09-07 19:40:08 +0600

    ADDED: workflow and tsup vercel.json

---
+++ apps/api/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/server.ts"],
+  format: ["cjs"],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  outDir: "dist",
+  external: ["@repo/utils", "@repo/prisma"],
+  noExternal: ["@repo/utils", "@repo/prisma"],
+});
